2b3f1fbdcb79039e29ab337f47e4907e93f6959b
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-08-13 13:59:06 +0300

    added some mixins

--- flask_test/__init__.py
+++ flask_test/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, json
+from flask import Flask, json, url_for
 from werkzeug import cached_property
 
 
@@ -13,8 +13,12 @@ class TestCase(object):
         """
         return Flask('test')
 
+    def after_create_app(self, app):
+        pass
+
     def setup_method(self, method):
         self.app = self.create_app()
+        self.after_create_app()
         self.client = self.app.test_client()
         self.xhr_client = xhr_test_client(self.app.test_client())
         self._ctx = self.app.test_request_context()
@@ -25,27 +29,29 @@ class TestCase(object):
         return self.app.extensions['sqlalchemy'].db
 
     def teardown_method(self, method):
-        self.client = None
-
-        try:
-            self.db.session.remove()
-            self._delete_tables()
-            self.db.session.close_all()
-            self.db.engine.dispose()
-            del self.app.extensions['sqlalchemy']
-            self._clear_sqlalchemy_event_listeners()
-        except KeyError:
-            pass
-
+        self.teardown_sqlalchemy()
         self._ctx.pop()
         self.app = None
+        self.client = None
         self.xhr_client = None
         self._ctx = None
 
         for key in self.__dict__.keys():
             setattr(self, key, None)
 
+    def teardown_sqlalchemy(self):
+        if hasattr(self, 'db'):
+            self.db.session.remove()
+            self._delete_tables()
+            self.db.session.close_all()
+            self.db.engine.dispose()
+            del self.app.extensions['sqlalchemy']
+            self._clear_sqlalchemy_event_listeners()
+
     def _delete_tables(self):
+        """
+        Deletes the data in all database tables
+        """
         tables = reversed(self.db.metadata.sorted_tables)
         for table in tables:
             self.db.session.execute(table.delete())
@@ -208,17 +214,159 @@ class TestCase(object):
 
 
 class JsonTestCase(TestCase):
+    def setup_method(self, method):
+        TestCase.setup_method(self, method)
+        self.current = Struct()
+
     def after_create_app(self):
         """
-        Create your Flask app here, with any configuration you need.
-
         Extends the normal meth:`create_app` by patching the response class to
         include a `json` attribute for quickly getting the response body as
         parsed as JSON.
         """
-        app = super(JsonTestCase, self).create_app()
-        app.response_class = _make_test_response(app.response_class)
-        return app
+        self.app.response_class = _make_test_response(self.app.response_class)
+        return self.app
+
+    def before_request(self):
+        pass
+
+    def _init_args(self, resource, **kwargs):
+        if resource is None:
+            resource = self.resource
+
+        if self.identifier not in kwargs:
+            kwargs[self.identifier] = getattr(self.current, resource).id
+        return resource, kwargs
+
+    def put(self, resource=None, data={}, **kwargs):
+        resource, kwargs = self._init_args(resource, **kwargs)
+
+        return self.xhr_client.put(
+            url_for(resource, _method='PUT', **kwargs),
+            data=data
+        )
+
+    def post(self, resource=None, data={}, **kwargs):
+        if resource is None:
+            resource = self.resource
+        response = self.xhr_client.post(
+            url_for(resource, _method='POST', **kwargs),
+            data=data
+        )
+        if response.status_code == 201:
+            setattr(self.current, resource, Struct(response.json['data']))
+        else:
+            setattr(self.current, resource, None)
+        return response
+
+    def delete(self, resource=None, **kwargs):
+        resource, kwargs = self._init_args(resource, **kwargs)
+        return self.xhr_client.delete(
+            url_for(
+                resource,
+                _method='DELETE',
+                **kwargs
+            )
+        )
+
+    def get(self, resource=None, query_string={}, **kwargs):
+        if resource is None:
+            resource = self.resource
+        return self.xhr_client.get(
+            url_for(
+                resource,
+                _method='GET',
+                **kwargs
+            )
+        )
+
+
+class CreateTestMixin(object):
+    def test_create_returns_201_on_success(self):
+        response = self.post()
+        self.assert201(response)
+
+    def test_create_returns_created_record_as_json_on_success(self):
+        response = self.post()
+        assert 'data' in response.json
+
+
+class IndexTestMixin(object):
+    def test_index_returns_200_on_success(self):
+        self.post()
+        response = self.get()
+        self.assert200(response)
+
+    def test_index_returns_list_of_json_objects_on_success(self):
+        self.post()
+        response = self.get()
+        assert len(response.json['data'])
+
+
+class UpdateTestMixin(object):
+    def test_update_returns_200_on_success(self):
+        self.post()
+        response = self.put()
+        self.assert200(response)
+
+    def test_update_returns_404_for_unknown_object(self):
+        response = self.put(**{self.identifier: 123123})
+        self.assert404(response)
+
+    def test_update_returns_updated_record_as_json_on_success(self):
+        self.post()
+        response = self.put()
+        assert 'data' in response.json
+
+
+class ShowTestMixin(object):
+    def test_show_returns_record_as_json_on_success(self):
+        self.post()
+        response = self.get()
+        assert 'data' in response.json
+
+    def test_show_returns_200_on_success(self):
+        self.post()
+        response = self.get()
+        self.assert200(response)
+
+    def test_show_returns_404_for_unknown_object(self):
+        response = self.get(**{self.identifier: 123123})
+        self.assert404(response)
+
+
+class DeleteTestMixin(object):
+    def test_delete_returns_204_on_success(self):
+        self.post()
+        response = self.delete()
+        self.assert204(response)
+
+    def test_delete_returns_404_for_unknown_object(self):
+        response = self.delete(**{self.identifier: 123123})
+        self.assert404(response)
+
+
+class Struct(object):
+    '''The recursive class for building and representing objects with.'''
+    def __init__(self, obj={}):
+        for k, v in obj.iteritems():
+            if isinstance(v, dict):
+                setattr(self, k, Struct(v))
+            else:
+                setattr(self, k, v)
+
+    def __getitem__(self, val):
+        return self.__dict__[val]
+
+    def __setitem__(self, key, val):
+        if isinstance(val, dict):
+            self.__dict__[key] = Struct(val)
+        else:
+            self.__dict__[key] = val
+
+    def __repr__(self):
+        return '{%s}' % str(', '.join('%s : %s' % (k, repr(v)) for
+               (k, v) in self.__dict__.iteritems()))
 
 
 class JsonResponseMixin(object):
