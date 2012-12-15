fabee30fb5310042443404129d78e56125f16792
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-12-15 11:14:40 +0200

    added Setup classes to main namespace

--- flask_test/__init__.py
+++ flask_test/__init__.py
@@ -1,20 +1,25 @@
 from .base import BaseTestCase, JsonResponseMixin
-from .integration import IntegrationTestCase, requires_login
+from .integration import IntegrationTestCase, IntegrationSetup, requires_login
 from .database import (
-    DatabaseMixin, DatabaseTestCase
+    DatabaseSetup, DatabaseMixin, DatabaseTestCase
 )
-from .view import ViewMixin, ViewTestCase, StaticPageMixin, StaticPageTestCase
+from .view import (
+    ViewMixin, ViewSetup, ViewTestCase, StaticPageMixin, StaticPageTestCase
+)
 
 
 __all__ = (
     BaseTestCase,
     DatabaseMixin,
+    DatabaseSetup,
     DatabaseTestCase,
+    IntegrationSetup,
     IntegrationTestCase,
     JsonResponseMixin,
+    requires_login,
     StaticPageMixin,
     StaticPageTestCase,
     ViewMixin,
+    ViewSetup,
     ViewTestCase,
-    requires_login,
 )
