2eb28ae99b8f2145200412d1add0a43540619244
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-12-15 14:04:04 +0200

    added ApplicationSetup to main module

--- flask_test/__init__.py
+++ flask_test/__init__.py
@@ -1,12 +1,13 @@
-from .base import TestCase, JsonResponseMixin, requires_login
+from .base import ApplicationSetup, TestCase, JsonResponseMixin, requires_login
 from database import DatabaseSetup
 from .view import ViewSetup
 
 
 __all__ = (
-    TestCase,
+    ApplicationSetup,
     DatabaseSetup,
     JsonResponseMixin,
     requires_login,
+    TestCase,
     ViewSetup,
 )
