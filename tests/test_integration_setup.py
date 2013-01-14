f5b870c861fb6105f5ca8fcc5e9d997b83d54193
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-01-14 10:27:13 +0200

    added travis.yml and renamed integration tests

---
+++ tests/test_integration_setup.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_test import TestCase
+from tests import TagAPI
+
+
+class User():
+    id = 1
+    email = u'someone@example.com'
+    password = u'someone'
+
+
+class TestIntegrationSetup(TestCase):
+    setup_level = 'class'
+
+    @classmethod
+    def create_app(cls):
+        app = Flask(__name__)
+        app.debug = True
+        app.secret_key = 'very secret'
+
+        tag_view = TagAPI.as_view('tag')
+        app.add_url_rule('/tags', defaults={'tag_id': None},
+                         view_func=tag_view, methods=['GET'])
+        app.add_url_rule('/tags', view_func=tag_view, methods=['POST'])
+        app.add_url_rule('/tags/<int:tag_id>', view_func=tag_view,
+                         methods=['GET', 'PUT', 'DELETE'])
+
+        return app
+
+    def test_method1(self):
+        assert self.app.secret_key == 'very secret'
+
+    def test_method2(self):
+        assert self.app.secret_key == 'very secret'
+
+    def create_or_get_user(self):
+        return User()
+
+    def test_login(self):
+        self.login()
